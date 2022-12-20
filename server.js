012d4ca4cd13bbd9db8a2a1d05e81a8c1fe35376
SmitKadawala <60181260+SmitKadawala@users.noreply.github.com>
2022-12-20 08:11:23 +0530

    changeport

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server Is Run On PORT ${PORT}`);
